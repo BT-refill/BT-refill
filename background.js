1b362445ee4b3f2c5f551bc26734fc4fce13d2cc
Refill <152452345+BT-refill@users.noreply.github.com>
2024-01-08 02:09:15 +0900

    Create background.js

---
+++ background.js
@@ -0,0 +1,8 @@
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "5.jpg", "6.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const image = new Image();
+image.src = `img/${chosenImage}`;
+image.classList.add("bgImage");
+document.body.appendChild(image);
